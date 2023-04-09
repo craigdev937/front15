fe05d37c07a0e20043b04ac32c3bdc788c301155
Craig <indigomx9@gmail.com>
2023-04-08 21:14:31 -0400

    Updated App and Header components.

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,12 +1,39 @@
 import React from "react";
 import "./Header.css";
+import Logo from "@public/logo.svg";
+import Mockup from "@public/illustration-mockups.svg";
 
 export const Header = () => {
     return (
         <React.Fragment>
             <header>
-                <section className="container">
-                    <h1>Header Text Goes Here</h1>
+                <section className="header__container">
+                    <aside className="header__top">
+                        <img
+                            className="header__logo"
+                            alt="Huddle Logo"  
+                            src={Logo}
+                        />
+                        <a 
+                            href="#" 
+                            className="header__button"
+                            >Try It Free
+                        </a>
+                    </aside>
+                    <aside className="header__hero">
+                        <section className="header__content">
+                            <h1>Build The Community Your Fans Will Love</h1>
+                            <p>
+                                Huddle re-imagines the way we 
+                                build communities. You have a voice, 
+                                but so does your audience. Create 
+                                connections with your users as you 
+                                engage in genuine discussion. 
+                            </p>
+                            <a href="#">Get Started For Free</a>
+                        </section>
+                    </aside>
+                    <img src={Mockup} alt="Mockup" />
                 </section>
             </header>
         </React.Fragment>
